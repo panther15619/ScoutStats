a0ce34f685edc7d765360d812ccc288c2d1b50f5
precious <121320466+panther15619@users.noreply.github.com>
2026-06-10 11:39:29 +0100

    Add saveProfile function to store player profile

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function saveProfile() {
 
+    const profile = {
+        name: document.getElementById("name").value,
+        age: document.getElementById("age").value,
+        position: document.getElementById("position").value,
+        team: document.getElementById("team").value
+    };
+
+    localStorage.setItem(
+        "playerProfile",
+        JSON.stringify(profile)
+    );
+
+    alert("Profile Saved!");
+}

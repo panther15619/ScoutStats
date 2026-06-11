ce2d7b77428ce704cabf6d0206249a9d1e8cc177
precious <121320466+panther15619@users.noreply.github.com>
2026-06-11 13:52:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ function saveProfile() {
     );
 
     alert("Profile Saved!");
-window.location.href = "dashboard.html";
+
+    window.location.href = "dashboard.html";
 }
 function saveMatch() {
 

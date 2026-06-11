ebefcbd454524d3d5d6a936a02322acfd8c6f671
precious <121320466+panther15619@users.noreply.github.com>
2026-06-11 13:42:00 +0100

    Add saveMatch function to store match data

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ function saveProfile() {
 
     alert("Profile Saved!");
 }
+function saveMatch() {
+
+    const match = {
+        opponent: document.getElementById("opponent").value,
+        competition: document.getElementById("competition").value,
+        minutes: document.getElementById("minutes").value,
+        goals: document.getElementById("goals").value,
+        assists: document.getElementById("assists").value,
+        shotsOnTarget: document.getElementById("shotsOnTarget").value,
+        shotsOffTarget: document.getElementById("shotsOffTarget").value,
+        oneVOneWon: document.getElementById("oneVOneWon").value,
+        oneVOneLost: document.getElementById("oneVOneLost").value,
+        reflection: document.getElementById("reflection").value
+    };
+
+    let matches =
+        JSON.parse(localStorage.getItem("matches")) || [];
+
+    matches.push(match);
+
+    localStorage.setItem(
+        "matches",
+        JSON.stringify(matches)
+    );
+
+    alert("Match Saved!");
+}

af77ea59fcd54ab6ea58e979448d30d47b0f7ecd
precious <121320466+panther15619@users.noreply.github.com>
2026-06-21 21:33:02 +0100

    Implement saveProfile function for player profiles
    Added saveProfile function to store player profile in localStorage and navigate to dashboard.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function saveProfile() {
+
+    const profile = {
+        name: document.getElementById("name").value,
+        age: document.getElementById("age").value,
+        position: document.getElementById("position").value,
+        team: document.getElementById("team").value
+    };
+
+    localStorage.setItem(
+        "playerProfile",
+        JSON.stringify(profile)
+    );
+
+    alert("Profile Saved!");
+
+    window.location.href = "./dashboard.html";
+}
+
 function saveMatch() {
 
     const goals =
@@ -18,15 +37,26 @@ function saveMatch() {
     const oneVOneLost =
     Number(document.getElementById("oneVOneLost").value);
 
+    const matchResult =
+    document.getElementById("result").value;
+
     let rating = 6.0;
 
     rating += goals * 1.0;
-    rating += assists * 0.7;
+    rating += assists * 0.8;
     rating += shotsOnTarget * 0.2;
-    rating += oneVOneWon * 0.1;
+    rating += oneVOneWon * 0.15;
 
     rating -= shotsOffTarget * 0.1;
     rating -= oneVOneLost * 0.15;
+
+    if (matchResult === "Win") {
+        rating += 0.3;
+    }
+
+    if (matchResult === "Loss") {
+        rating -= 0.3;
+    }
 
     if (rating > 10) rating = 10;
     if (rating < 1) rating = 1;
@@ -38,6 +68,9 @@ function saveMatch() {
 
         competition:
         document.getElementById("competition").value,
+
+        result:
+        document.getElementById("result").value,
 
         matchDate:
         document.getElementById("matchDate").value,
@@ -74,4 +107,6 @@ function saveMatch() {
         "Match Saved!\n\nRating: " +
         rating.toFixed(1)
     );
+
+    window.location.href = "dashboard.html";
 }
